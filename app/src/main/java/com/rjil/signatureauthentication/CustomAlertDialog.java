3c96560c38cdd9fe8ef42fdbd208f7d990f42de4
JitendraAndroid <jitendra.khetle@gmail.com>
2016-11-28 13:27:23 +0530

    Initial Commit

---
+++ app/src/main/java/com/rjil/signatureauthentication/CustomAlertDialog.java
@@ -0,0 +1,253 @@
+package com.rjil.signatureauthentication;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.text.TextUtils;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class CustomAlertDialog extends DialogFragment implements
+        OnClickListener {
+    public static final String MESSAGE = "message";
+    public static final String TITLE = "title";
+    public static final String IS_ALERT = "is_alert";
+    public static final String IS_CHECK_BOX = "is_check_box";
+    public static final String IS_CHECK_BOX_MSG = "is_check_box_msg";
+    public static final String IS_RESULT_IMAGE = "is_image";
+    private CharSequence mMsg;
+    private CharSequence mTitle;
+    private CharSequence mNegativeBtnTxt;
+    private CharSequence mPositiveBtnTxt;
+    private boolean isAlert = true;
+    private ImageView ivIcon;
+    private View mLlCheckBox;
+    private boolean isCheckBoxVisible;
+    private String mCheckBoxMsg;
+    private ImageView mImageView;
+    private CheckBox mChkBox;
+    private View mView;
+    private Bitmap mResultBitmap;
+
+    public enum Type {
+        BUTTON_POSITIVE,
+        BUTTON_NEGATIVE,
+        BUTTON_POSITIVE_CHECKED
+    }
+
+    private TextView mDialogMsg;
+    private TextView mDialogTitle;
+    private Button mBtnPositive;
+    private Button mBtnNegative;
+    private OnCustomDialogClickListener btnNegativeListener;
+    private OnCustomDialogClickListener btnPositiveListener;
+
+    public CustomAlertDialog() {
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NO_TITLE, R.style.Translucent_ProgressDialog);
+
+        Bundle arguments = getArguments();
+        if (arguments != null) {
+            mMsg = arguments.getString(MESSAGE);
+            mTitle = arguments.getString(TITLE);
+            isAlert = arguments.getBoolean(IS_ALERT);
+            isCheckBoxVisible = arguments.getBoolean(IS_CHECK_BOX);
+            mCheckBoxMsg = arguments.getString(IS_CHECK_BOX_MSG);
+            mResultBitmap = arguments.getParcelable(IS_RESULT_IMAGE);
+        }
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        return mView = inflater.inflate(R.layout.custom_alert_dialog_view, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        getDialog().getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        initView();
+        initMembers();
+        initData();
+        initListener();
+        initOthers();
+    }
+
+    private void initOthers() {
+        Handler mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (isAlert)
+                    setIcon(R.drawable.alert);
+                else
+                    setIcon(R.drawable.success);
+            }
+        }, 400);
+
+    }
+
+    private void initListener() {
+        setPositiveButton(mPositiveBtnTxt, btnPositiveListener);
+        setNegativeButton(mNegativeBtnTxt, btnNegativeListener);
+    }
+
+    private void initData() {
+
+    }
+
+    private void initMembers() {
+        setWidth(80);
+        setTitle(mTitle);
+        setMessage(mMsg);
+        setImage(mResultBitmap);
+    }
+
+    private void setImage(Bitmap mResultBitmap) {
+        mImageView.setImageBitmap(mResultBitmap);
+    }
+
+    protected void setWidth(int widthPercent) {
+        int deviceWidth = deviceWidth();
+        int deviceHeight = deviceHeight();
+        if (deviceWidth > deviceHeight) {
+            deviceWidth = deviceHeight;
+        }
+
+        int dialogWidth = (deviceWidth * widthPercent) / 100;
+        getDialog().getWindow().setLayout(dialogWidth, ViewGroup.LayoutParams.WRAP_CONTENT);
+    }
+
+    public CustomAlertDialog setMessage(CharSequence message) {
+        mMsg = message;
+        if (mDialogMsg != null) {
+            mDialogMsg.setText(mMsg);
+        }
+        return this;
+    }
+
+    private void setIcon(int icon) {
+        Animation anim = AnimationUtils.loadAnimation(getActivity(), R.anim.scale_open);
+        anim.setInterpolator(getActivity(), android.R.anim.bounce_interpolator);
+        ivIcon.setImageResource(icon);
+        ivIcon.setVisibility(View.VISIBLE);
+        ivIcon.startAnimation(anim);
+    }
+
+    public CustomAlertDialog setPositiveButton(CharSequence text,
+                                               OnCustomDialogClickListener listener) {
+
+        btnPositiveListener = listener;
+        mPositiveBtnTxt = text;
+        if (mBtnPositive != null) {
+            mBtnPositive.setOnClickListener(CustomAlertDialog.this);
+            if (!TextUtils.isEmpty(mPositiveBtnTxt))
+                mBtnPositive.setText(text);
+        }
+        return this;
+    }
+
+    public CustomAlertDialog setNegativeButton(CharSequence text,
+                                               OnCustomDialogClickListener listener) {
+        btnNegativeListener = listener;
+        mNegativeBtnTxt = text;
+        if (mBtnNegative != null && btnNegativeListener != null) {
+            mBtnNegative.setVisibility(View.VISIBLE);
+            mBtnNegative.setText(mNegativeBtnTxt);
+            mBtnNegative.setOnClickListener(CustomAlertDialog.this);
+        }
+        return this;
+    }
+
+    public CustomAlertDialog setTitle(CharSequence title) {
+        mTitle = title;
+        if (mDialogTitle != null) {
+            mDialogTitle.setText(mTitle);
+        }
+        return this;
+    }
+
+
+    private void initView() {
+        mDialogMsg = (TextView) mView.findViewById(R.id.tv_alert_msg);
+        mImageView = (ImageView)mView.findViewById(R.id.resultImage);
+/*        mChkBox = (CheckBox) mView.findViewById(R.id.chkbox_alert);
+        mLlCheckBox = mView.findViewById(R.id.ll_checkbox);*/
+        mDialogTitle = (TextView) mView
+                .findViewById(R.id.tv_alert_header);
+        mBtnPositive = (Button) mView.findViewById(R.id.btn_positive);
+        mBtnNegative = (Button) mView.findViewById(R.id.btn_negative);
+        ivIcon = (ImageView) mView.findViewById(R.id.iv_header);
+
+        if (isCheckBoxVisible) {
+            mLlCheckBox.setVisibility(View.VISIBLE);
+            mChkBox.setText(mCheckBoxMsg);
+        }
+    }
+
+    @Override
+    public void setCancelable(boolean cancelable) {
+        super.setCancelable(cancelable);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_positive) {
+            if (btnPositiveListener != null) {
+                if (isCheckBoxVisible && mChkBox.isChecked()) {
+                    btnPositiveListener.onCustomDialogClick(Type.BUTTON_POSITIVE_CHECKED);
+                } else {
+                    btnPositiveListener.onCustomDialogClick(Type.BUTTON_POSITIVE);
+                }
+            }
+            dismiss();
+        } else {
+            if (btnNegativeListener != null) {
+                btnNegativeListener.onCustomDialogClick(Type.BUTTON_NEGATIVE);
+            }
+            dismiss();
+        }
+    }
+
+    public int deviceWidth() {
+        return getDisplayMetrics().widthPixels;
+    }
+
+    public int deviceHeight() {
+        return getDisplayMetrics().heightPixels;
+    }
+
+
+    private DisplayMetrics getDisplayMetrics() {
+        WindowManager wm = (WindowManager) getActivity()
+                .getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        return metrics;
+    }
+}
---
+++ app/src/main/java/com/rjil/signatureauthentication/MainActivity.java
@@ -0,0 +1,380 @@
+package com.rjil.signatureauthentication;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfDMatch;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfKeyPoint;
+import org.opencv.core.Scalar;
+import org.opencv.features2d.DMatch;
+import org.opencv.features2d.DescriptorExtractor;
+import org.opencv.features2d.DescriptorMatcher;
+import org.opencv.features2d.FeatureDetector;
+import org.opencv.features2d.Features2d;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnCaptureModel, btnCaptureSignature, btnAuthenticate;
+    private ImageView imgCaptureModel, imgCaptureSignature;
+    private static Bitmap bmp;
+    private static Bitmap bmpimg1;
+    private static Bitmap bmpimg2;
+    private Bitmap yourSelectedImage;
+    private static final int SELECT_PHOTO = 100;
+    private String TAG = getClass().getSimpleName();
+    private Uri selectedImage;
+    private int imgNo = 0;
+    private InputStream imageStream;
+
+    private String path1, path2;
+    /**
+     * Callback handler for application OpenCV Manager install
+     */
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        initComponent();
+        initListener();
+    }
+
+
+    private void initComponent() {
+        btnCaptureModel = (Button) findViewById(R.id.btn_capture_model);
+        btnCaptureSignature = (Button) findViewById(R.id.btn_capture_signature);
+        imgCaptureModel = (ImageView) findViewById(R.id.img_capture_model);
+        imgCaptureSignature = (ImageView) findViewById(R.id.img_capture_signature);
+        btnAuthenticate = (Button) findViewById(R.id.btn_authenticate);
+
+    }
+
+    private void initListener() {
+        btnCaptureModel.setOnClickListener(this);
+        btnCaptureSignature.setOnClickListener(this);
+        btnAuthenticate.setOnClickListener(this);
+        imgCaptureSignature.setOnClickListener(this);
+        imgCaptureModel.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //Call for OpenCV Manager application to load.
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_4, this,
+                mLoaderCallback);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.img_capture_model:
+            case R.id.btn_capture_model: {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+                imgNo = 1;
+            }
+            break;
+            case R.id.img_capture_signature:
+            case R.id.btn_capture_signature: {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+                imgNo = 2;
+            }
+            break;
+            case R.id.btn_authenticate: {
+                authenticateSignature();
+            }
+            break;
+        }
+    }
+
+    private void authenticateSignature() {
+        if (bmpimg1 != null && bmpimg2 != null) {
+            bmpimg1 = Bitmap.createScaledBitmap(bmpimg1, 100, 100, true);
+            bmpimg2 = Bitmap.createScaledBitmap(bmpimg2, 100, 100, true);
+            Mat img1 = new Mat();
+            Utils.bitmapToMat(bmpimg1, img1);
+            Mat img2 = new Mat();
+            Utils.bitmapToMat(bmpimg2, img2);
+            Imgproc.cvtColor(img1, img1, Imgproc.COLOR_RGBA2GRAY);
+            Imgproc.cvtColor(img2, img2, Imgproc.COLOR_RGBA2GRAY);
+            img1.convertTo(img1, CvType.CV_32F);
+            img2.convertTo(img2, CvType.CV_32F);
+            Mat hist1 = new Mat();
+            Mat hist2 = new Mat();
+            MatOfInt histSize = new MatOfInt(180);
+            MatOfInt channels = new MatOfInt(0);
+            ArrayList<Mat> bgr_planes1 = new ArrayList<Mat>();
+            ArrayList<Mat> bgr_planes2 = new ArrayList<Mat>();
+            Core.split(img1, bgr_planes1);
+            Core.split(img2, bgr_planes2);
+            MatOfFloat histRanges = new MatOfFloat(0f, 180f);
+            boolean accumulate = false;
+            Imgproc.calcHist(bgr_planes1, channels, new Mat(), hist1, histSize, histRanges, accumulate);
+            Core.normalize(hist1, hist1, 0, hist1.rows(), Core.NORM_MINMAX, -1, new Mat());
+            Imgproc.calcHist(bgr_planes2, channels, new Mat(), hist2, histSize, histRanges, accumulate);
+            Core.normalize(hist2, hist2, 0, hist2.rows(), Core.NORM_MINMAX, -1, new Mat());
+            img1.convertTo(img1, CvType.CV_32F);
+            img2.convertTo(img2, CvType.CV_32F);
+            hist1.convertTo(hist1, CvType.CV_32F);
+            hist2.convertTo(hist2, CvType.CV_32F);
+
+            double compare = Imgproc.compareHist(hist1, hist2, Imgproc.CV_COMP_CHISQR);
+            Log.d("ImageComparator", "compare: " + compare);
+            if (compare > 0 && compare < 10000) {
+                //Toast.makeText(MainActivity.this, "Images may be possible duplicates, verifying", Toast.LENGTH_SHORT).show();
+                new asyncTask(MainActivity.this).execute();
+            } else if (compare == 0)
+                Toast.makeText(MainActivity.this, "Images are exact duplicates", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(MainActivity.this, "Images are not duplicates", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(MainActivity.this,
+                    "You haven't selected images.", Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+        switch (requestCode) {
+            case SELECT_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    selectedImage = imageReturnedIntent.getData();
+                    try {
+                        imageStream = getContentResolver().openInputStream(
+                                selectedImage);
+                    } catch (FileNotFoundException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    //yourSelectedImage = decodeSampledBitmapFromStream(imageStream, imgCaptureModel.getHeight(), imgCaptureModel.getWidth());
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inSampleSize = 4;//calculateInSampleSize(options, imgCaptureModel.getWidth(), imgCaptureModel.getHeight());
+
+                    yourSelectedImage = BitmapFactory.decodeStream(imageStream, null, options);
+
+                    if (imgNo == 1) {
+                        imgCaptureModel.setImageBitmap(yourSelectedImage);
+                        path1 = selectedImage.getPath();
+                        bmpimg1 = yourSelectedImage;
+                        imgCaptureModel.invalidate();
+
+                    } else if (imgNo == 2) {
+                        imgCaptureSignature.setImageBitmap(yourSelectedImage);
+                        path2 = selectedImage.getPath();
+                        bmpimg2 = yourSelectedImage;
+                        imgCaptureSignature.invalidate();
+                    }
+                }
+        }
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+
+    public static class asyncTask extends AsyncTask<Void, Void, Void> implements OnCustomDialogClickListener {
+        private static Mat img1, img2, descriptors, dupDescriptors;
+        private static FeatureDetector detector;
+        private static DescriptorExtractor DescExtractor;
+        private static DescriptorMatcher matcher;
+        private static MatOfKeyPoint keypoints, dupKeypoints;
+        private static MatOfDMatch matches, matches_final_mat;
+        private static ProgressDialog pd;
+        private static boolean isDuplicate = false;
+        private MainActivity asyncTaskContext = null;
+        private static Scalar RED = new Scalar(255, 0, 0);
+        private static Scalar GREEN = new Scalar(0, 255, 0);
+        private int min_dist = 100;
+        private int min_matches = 20;
+        String message;
+
+        public asyncTask(MainActivity context) {
+            asyncTaskContext = context;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            pd = new ProgressDialog(asyncTaskContext);
+            pd.setIndeterminate(true);
+            pd.setCancelable(true);
+            pd.setCanceledOnTouchOutside(false);
+            pd.setMessage("Images may be possible duplicates, verifying...");
+            pd.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            // TODO Auto-generated method stub
+            compare();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            try {
+                Mat img3 = new Mat();
+                MatOfByte drawnMatches = new MatOfByte();
+                Features2d.drawMatches(img1, keypoints, img2, dupKeypoints,
+                        matches_final_mat, img3, GREEN, RED, drawnMatches, Features2d.NOT_DRAW_SINGLE_POINTS);
+                bmp = Bitmap.createBitmap(img3.cols(), img3.rows(),
+                        Bitmap.Config.ARGB_8888);
+                Imgproc.cvtColor(img3, img3, Imgproc.COLOR_BGR2RGB);
+                Utils.matToBitmap(img3, bmp);
+                Bitmap resize  = Bitmap.createScaledBitmap(bmp,(int)(bmp.getWidth()*2), (int)(bmp.getHeight()*2), true);
+                List<DMatch> finalMatchesList = matches_final_mat.toList();
+                final int matchesFound = finalMatchesList.size();
+                if (finalMatchesList.size() >= min_matches)// dev discretion for
+                // number of matches to
+                // be found for an image
+                // to be judged as
+                // duplicate
+
+                {
+                    message = finalMatchesList.size() + " matches were found, Signature can be authenticated";
+                    DialogUtils.showDialog(asyncTaskContext, "Results", message, "", "", this, false, resize);
+                } else {
+                    message = "Signature not authenticated (matches : "+finalMatchesList.size()+")";
+                    DialogUtils.showDialog(asyncTaskContext, "Results", message, "", "", this, true, resize);
+                }
+                pd.dismiss();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(asyncTaskContext, e.toString(),
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+
+        void compare() {
+            try {
+                bmpimg1 = bmpimg1.copy(Bitmap.Config.ARGB_8888, true);
+                bmpimg2 = bmpimg2.copy(Bitmap.Config.ARGB_8888, true);
+                img1 = new Mat();
+                img2 = new Mat();
+                Utils.bitmapToMat(bmpimg1, img1);
+                Utils.bitmapToMat(bmpimg2, img2);
+                Imgproc.cvtColor(img1, img1, Imgproc.COLOR_BGR2RGB);
+                Imgproc.cvtColor(img2, img2, Imgproc.COLOR_BGR2RGB);
+                detector = FeatureDetector.create(FeatureDetector.PYRAMID_FAST);
+                DescExtractor = DescriptorExtractor.create(DescriptorExtractor.BRISK);
+                matcher = DescriptorMatcher
+                        .create(DescriptorMatcher.BRUTEFORCE_HAMMING);
+
+                keypoints = new MatOfKeyPoint();
+                dupKeypoints = new MatOfKeyPoint();
+                descriptors = new Mat();
+                dupDescriptors = new Mat();
+                matches = new MatOfDMatch();
+                detector.detect(img1, keypoints);
+                Log.d("LOG!", "number of query Keypoints= " + keypoints.size());
+                detector.detect(img2, dupKeypoints);
+                Log.d("LOG!", "number of dup Keypoints= " + dupKeypoints.size());
+                // Descript keypoints
+                DescExtractor.compute(img1, keypoints, descriptors);
+                DescExtractor.compute(img2, dupKeypoints, dupDescriptors);
+                Log.d("LOG!", "number of descriptors= " + descriptors.size());
+                Log.d("LOG!",
+                        "number of dupDescriptors= " + dupDescriptors.size());
+                // matching descriptors
+                matcher.match(descriptors, dupDescriptors, matches);
+                Log.d("LOG!", "Matches Size " + matches.size());
+                // New method of finding best matches
+                List<DMatch> matchesList = matches.toList();
+                List<DMatch> matches_final = new ArrayList<DMatch>();
+                for (int i = 0; i < matchesList.size(); i++) {
+                    if (matchesList.get(i).distance <= min_dist) {
+                        matches_final.add(matches.toList().get(i));
+                    }
+                }
+
+                matches_final_mat = new MatOfDMatch();
+                matches_final_mat.fromList(matches_final);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        @Override
+        public void onCustomDialogClick(CustomAlertDialog.Type type) {
+
+        }
+    }
+
+}
